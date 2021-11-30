3f3973e5875f2b59e350f16b2bb31ce82f1abb78
kylercantu <92545482+kylercantu@users.noreply.github.com>
2021-11-30 12:26:50 -0500

    Update ConsentPage.java

--- NewOpTime/src/newoptime/ConsentPage.java
+++ NewOpTime/src/newoptime/ConsentPage.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import javax.swing.*;
 
 public class ConsentPage implements ActionListener{
