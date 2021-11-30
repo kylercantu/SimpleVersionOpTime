f06ff6418d73c7169d36a19a27bb88ba5a65df3f
kylercantu <92545482+kylercantu@users.noreply.github.com>
2021-11-30 16:00:30 -0500

    Add files via upload

---
+++ NewOpTime/src/newoptime/StopWatch.java
@@ -0,0 +1,45 @@
+
+package newoptime;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+
+
+public class StopWatch{
+    
+    int elapsedTime = 0;
+    int minutes =0;
+    int hours = 0;
+    boolean started = false;
+    
+    Timer timer = new Timer(1000, new ActionListener() {
+        
+        public void actionPerformed(ActionEvent e){
+            
+            elapsedTime += 1000;
+            hours = (elapsedTime/3600000);
+            minutes = (elapsedTime/60000) % 60;
+            String minutes_string = String.format("%02d", minutes);
+            String hours_string = String.format("%02d", hours);
+        }
+        
+        
+    });
+    
+    void start(){
+        timer.start();
+    }
+    
+    void stop(){
+        timer.stop();
+    }
+    
+
+
+    
+
+    
+}//End stopWatch
